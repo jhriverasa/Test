9d6abd189c29b1316a8d1e0a487bae286e46a100
JhonatanR. Saumeth <jhriverasa@unal.edu.co>
2019-12-21 22:43:36 -0500

    added google authorization and listing events from primary calendar"

---
+++ src/components/Calendar/Event.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+//import Container from 'react-bootstrap/Container'
+//import Row from 'react-bootstrap/Row'
+//import Col from 'react-bootstrap/Col'
+import './Event-styles.css';
+
+export class Event extends Component {
+
+    
+    render() {
+        return (
+           
+                        <div className="eventContainer">
+                            <h3>{this.props.summary}</h3>
+                            <p>Start: {this.props.start} - End: {this.props.end}</p>
+                            <p>Creator: {this.props.creator}</p>
+                        </div>
+         
+        )
+    }
+}
+
+export default Event
